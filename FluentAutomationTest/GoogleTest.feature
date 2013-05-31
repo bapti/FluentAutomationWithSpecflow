1f18fade113c577e4204effd3cfb7a5ca519a462
Neil Crawford <neil.crawford@gmail.com>
2013-05-31 16:50:38 +0100

    Initial Commit

---
+++ FluentAutomationTest/GoogleTest.feature
@@ -0,0 +1,9 @@
+﻿Feature: GoogleTest
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@Web
+Scenario: Go to google
+	Given I go to google
+	Then I should be at google
---
+++ FluentAutomationTest/TestSetup.cs
@@ -0,0 +1,67 @@
+﻿using BoDi;
+using FluentAutomation.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace FluentAutomationTest
+{
+    [Binding]
+    public class WebScenario : FluentAutomation.FluentTest
+    {
+        private readonly IObjectContainer objectContainer;
+
+        public WebScenario(IObjectContainer objectContainer)
+        {
+            this.objectContainer = objectContainer;
+
+            FluentAutomation.Settings.ScreenshotPath = @"C:\Work\Temp";
+            FluentAutomation.Settings.ScreenshotOnFailedExpect = false;
+            FluentAutomation.Settings.ScreenshotOnFailedAction = false;
+            FluentAutomation.Settings.DefaultWaitTimeout = TimeSpan.FromSeconds(1);
+            FluentAutomation.Settings.DefaultWaitUntilTimeout = TimeSpan.FromSeconds(30);
+            FluentAutomation.Settings.MinimizeAllWindowsOnTestStart = true;
+        }
+
+        [BeforeScenario("Web")]
+        public void BeforeScenario()
+        {
+            FluentAutomation.SeleniumWebDriver.Bootstrap(FluentAutomation.SeleniumWebDriver.Browser.Firefox);
+
+            objectContainer.RegisterInstanceAs<INativeActionSyntaxProvider>(I);
+        }
+
+        [AfterScenario("Web")]
+        public void AfterScenario()
+        {
+
+        }
+    }
+
+    [Binding]
+    public class GoogleSteps
+    {
+        public INativeActionSyntaxProvider I { get; set; }
+        
+        public GoogleSteps(INativeActionSyntaxProvider I)
+        {
+            this.I = I;
+        }
+
+        [Given(@"I go to google")]
+        public void GivenIGoToGoogle()
+        {
+            I.Open("http://google.co.uk");
+        }
+
+        [Then(@"I should be at google")]
+        public void ThenIShouldBeAtGoogle()
+        {
+            I.Expect.Url(x => x.AbsoluteUri.Contains("google"));
+        }
+
+    }
+}
